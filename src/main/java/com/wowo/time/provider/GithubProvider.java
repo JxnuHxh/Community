c5bd5b461204519f7c11445c5484853a6d098ea6
hxh <617662163@qq.com>
2019-09-22 23:11:37 +0800

    test

--- src/main/java/com/wowo/time/provider/GithubProvider.java
+++ src/main/java/com/wowo/time/provider/GithubProvider.java
@@ -1,11 +1,29 @@
 package com.wowo.time.provider;
 
+import com.wowo.time.dto.AccessTokenDTO;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.xml.ws.Response;
+import java.io.IOException;
 
 @Component
 public class GithubProvider {
-    public static void main(String[] args) {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO)
+    {
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(json, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
+
 
 }
