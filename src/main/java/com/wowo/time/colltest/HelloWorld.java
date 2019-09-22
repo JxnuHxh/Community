30b50931775d481a3193b37a931bd5bb21980d60
hxh <617662163@qq.com>
2019-09-22 14:29:19 +0800

    增加了导航栏

--- src/main/java/com/wowo/time/colltest/HelloWorld.java
+++ src/main/java/com/wowo/time/colltest/HelloWorld.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorld {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
     }
 
 }
