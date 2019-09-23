31fb75af633f154060df1ff402738e2df019f04b
hxh <617662163@qq.com>
2019-09-23 11:27:52 +0800

    完成显示用户名

--- src/main/java/com/wowo/time/controller/AuthorizeController.java
+++ src/main/java/com/wowo/time/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -24,7 +26,8 @@ public class AuthorizeController {
     private String redirectUri;
      @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code ,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                             HttpServletRequest request ){
         AccessTokenDTO accessTokenDTO= new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRediret_uri(redirectUri);
@@ -33,8 +36,17 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken= githubProvider.getAccessToken(accessTokenDTO);
          GithubUser user=githubProvider.getUser(accessToken);
-         System.out.println(user.getName());
-        return "index";
+         System.out.println(user);
+         if(user!=null)
+         {
+             //登录成功
+             request.getSession().setAttribute("user",user);
+             return "redirect:/";
+
+         }else {
+             //登录失败
+             return "redirect:/";
+         }
 
     }
 }
