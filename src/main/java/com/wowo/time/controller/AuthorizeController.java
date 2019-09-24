c2039919a849ab99a5daf0fb869d0049abeb14f6
hxh <617662163@qq.com>
2019-09-24 09:03:27 +0800

    cookie

--- src/main/java/com/wowo/time/controller/AuthorizeController.java
+++ src/main/java/com/wowo/time/controller/AuthorizeController.java
@@ -5,13 +5,17 @@ import com.wowo.time.dto.GithubUser;
 import com.wowo.time.mapper.UserMapper;
 import com.wowo.time.model.User;
 import com.wowo.time.provider.GithubProvider;
+import jdk.nashorn.internal.parser.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +37,8 @@ public class AuthorizeController {
      @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code ,
                            @RequestParam(name="state")String state,
-                             HttpServletRequest request ){
+                             HttpServletRequest request ,
+                              HttpServletResponse Response){
         AccessTokenDTO accessTokenDTO= new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRediret_uri(redirectUri);
@@ -47,13 +52,15 @@ public class AuthorizeController {
          {
              //登录成功
             User user=new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token=UUID.randomUUID().toString();
+            user.setToken(token);
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setName(githubUser.getName());
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtModified());
 
              userMapper.insert(user);
+            Response.addCookie(new Cookie("token", token));
              request.getSession().setAttribute("user",githubUser);
              return "redirect:/";
 
--- src/main/java/com/wowo/time/mapper/UserMapper.java
+++ src/main/java/com/wowo/time/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.wowo.time.mapper;
 import com.wowo.time.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -10,4 +12,6 @@ public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})" )
  void insert(User user);
 
+    @Select("select* from user where token=#{token}")
+    User findByYoken(@Param("token") String token);
 }
